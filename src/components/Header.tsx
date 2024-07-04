f5a436a1587fcd3c0c3e7de4005783f8fe8303c2
Ana Barbosa <ana28.98@hotmail.com>
2024-07-04 11:13:46 +0100

    add scroll top on logo click

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,18 @@ import MBD from "../assets/svg/logo.svg";
 
 const Header: React.FunctionComponent = () => {
 
+  const onClickLogo = () => {
+    if (window.scrollY > 0) {
+      window.scrollTo({ top: 0, behavior: "smooth" })
+    }
+  }
+
   return (
     <header className='header'>
     <div className='container'>
+      <button onClick={onClickLogo}>
         <MBD />
+      </button>
     </div>
     </header>
   )
